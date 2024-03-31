9d40fb25a7c76aee2ec966651d370176eae4cc3e
Dishank Shekokare <99533410+Dishank25S@users.noreply.github.com>
2024-04-01 00:51:32 +0530

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Dishank Shekokare",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "Dishank.Shekokare@studentambassadors.com",
+  gitHub: "Dishank25S",
+  instagram: "",
+  linkedIn: "dishank-shekokare",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "DishankShekokare",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
